49ea88299ae0d86fe115c0031636b169edb49d09
Bianca Miranda <miranda.bianca41@gmail.com>
2024-09-16 14:51:21 -0300

    Correção de Bugs

--- PotyguaraGame/Assets/Dragao-20240909T162717Z-001/Dragao/DragonController.cs
+++ PotyguaraGame/Assets/Dragao-20240909T162717Z-001/Dragao/DragonController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils.Bindings.Variables;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Rendering;
@@ -7,10 +8,16 @@ using UnityEngine.Rendering;
 public class DragonController : MonoBehaviour
 {
     public Transform[] ways;
+
     private Transform currentIAPoint;
     private int count = 0;
     private Animator ani;
     private float speed = 10;
+    private bool startCurve = false;
+    private int numVoltas = 2;
+    private float currentValueCurve = 0f;
+    private bool startDragon = true;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,35 +28,77 @@ public class DragonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float distanceForAIPoint = Vector3.Distance(currentIAPoint.position, transform.position);
-        if(distanceForAIPoint < 2f)
+        if (startDragon)
         {
-            if (count == ways.Length - 1)
+            float distanceForAIPoint = Vector3.Distance(currentIAPoint.position, transform.position);
+            if (distanceForAIPoint < 0.4f)
             {
-                count = 0;
+                if (count == ways.Length - 1)
+                {
+                    numVoltas--;
+                    if (numVoltas == 0)
+                    {
+                        count = 0;
+                        Destroy(gameObject, 15f);
+                    }
+                    else
+                    {
+                        count = 3;
+                    }
+                }
+                else
+                {
+                    count++;
+                }
+                currentIAPoint = ways[count];
             }
-            else
-            {
-                count++;
-            }
-            currentIAPoint = ways[count];
+            Flying();
         }
-        Flying();
     }
 
     private void Flying()
     {
         ani.SetBool("isFlying", true);
         ani.SetBool("isRunning", false);
-        if(currentIAPoint.position.x > transform.position.x){
-            transform.eulerAngles = Vector3.MoveTowards(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y - 90f/*90f*/, 0), 30 * Time.deltaTime);
-        }
-        else if(currentIAPoint.position.x < transform.position.x){
-            transform.eulerAngles = Vector3.MoveTowards(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y+90f/*270*/, 0), 30 * Time.deltaTime);
-        }else if (currentIAPoint.position.z > transform.position.z)
+        if (numVoltas == 0)
         {
-            transform.eulerAngles = Vector3.MoveTowards(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + 90f/*360*/, 0), 30 * Time.deltaTime);
+            if (transform.eulerAngles.y > currentValueCurve - 180f)
+            {
+                transform.eulerAngles = Vector3.MoveTowards(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y - 90f, 0), 30 * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (startCurve)
+            {
+                if (transform.eulerAngles.y < currentValueCurve)
+                {
+                    transform.eulerAngles = Vector3.MoveTowards(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + 90f, 0), 30 * Time.deltaTime);
+                }
+                Invoke("setBool", 3f);
+            }
         }
         transform.position = Vector3.MoveTowards(transform.position, currentIAPoint.position, speed*Time.deltaTime);
     }
+
+    private void setBool()
+    {
+        startCurve = false;
+    }
+
+    public void setStartDragon(bool value)
+    {
+        startDragon = value;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Colisor!!");
+        if (other.tag.Equals("pointCurve"))
+        {
+            startCurve = true;
+            currentValueCurve = transform.eulerAngles.y+90;
+        }
+    }
+
 }
