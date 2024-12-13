9cb5767564f977ad139f8c731908d00a29b0c2f5
Bianca Miranda <miranda.bianca41@gmail.com>
2024-12-13 19:34:14 -0300

    .

--- PotyguaraGame/Assets/Scripts/Forte/TargetController.cs
+++ PotyguaraGame/Assets/Scripts/Forte/TargetController.cs
@@ -6,15 +6,26 @@ using UnityEngine.UI;
 public class TargetController : MonoBehaviour
 {
     public bool receivedDamage = false;
-
-    private void Update()
+    [SerializeField] private Vector3 startPoint;
+    [SerializeField] private Vector3 endPoint;
+    [SerializeField] private float speed = 0.3f;
+    void Start()
     {
-        transform.position = Mathf.Lerp(Mathf.PingPong());
+        startPoint = transform.position;
+        endPoint = new Vector3(transform.position.x, transform.position.y, 390.1875f);
+    }
+    void Update()
+    {
+        if (startPoint != null && endPoint != null)
+        {
+            float pp = Mathf.PingPong(Time.deltaTime * speed, 1f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Lerp(startPoint.z, endPoint.z, 5f));
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Body"))
+        if (collision.gameObject.CompareTag("Bullet"))
         {
             if (!receivedDamage)
             {
