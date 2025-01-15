88169c7baed71a0aff9746a480cfb7b74b80603d
Bianca Miranda <miranda.bianca41@gmail.com>
2025-01-15 12:08:44 -0300

    Teleport + Ocean sound

--- PotyguaraGame/Assets/Scripts/AudioController.cs
+++ PotyguaraGame/Assets/Scripts/AudioController.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    public Transform player;
+    private Transform player;
 
     private float distanceToPlayer;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindWithTag("Player").transform;
         GetComponent<AudioSource>().Play();
     }
 
@@ -19,16 +20,16 @@ public class AudioController : MonoBehaviour
         distanceToPlayer = Vector3.Distance(player.position, transform.position);
         if(distanceToPlayer > 40)
         {
-            GetComponent<AudioSource>().volume = 0.15f;
+            GetComponent<AudioSource>().volume = 0.1f;
         }else if(distanceToPlayer > 20)
         {
-            GetComponent<AudioSource>().volume = 0.25f;
+            GetComponent<AudioSource>().volume = 0.2f;
         }else if(distanceToPlayer > 10)
         {
-            GetComponent<AudioSource>().volume = 0.50f;
+            GetComponent<AudioSource>().volume = 0.45f;
         }else if(distanceToPlayer > 0)
         {
-            GetComponent<AudioSource>().volume = 0.75f;
+            GetComponent<AudioSource>().volume = 0.7f;
         }
     }
 }
