0b87d0a78136ad6c7294689e94da81cb86204fca
Bianca Miranda <99360129+Bianca-Mirtes@users.noreply.github.com>
2025-07-03 03:09:29 -0300

    Update TransitionController.cs

--- PotyguaraGame/Assets/Scripts/TransitionController.cs
+++ PotyguaraGame/Assets/Scripts/TransitionController.cs
@@ -66,6 +66,19 @@ public class TransitionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            if (!menuUpdated)
+            {
+                NetworkManager net = NetworkManager.Instance;
+                if(net != null)
+                {
+                    UpdateMainMenu(net.isTheFirstAcess);
+                    menuUpdated = true;
+                }
+            }
+        }
+        
         if (isSkip && SceneManager.GetActiveScene().buildIndex == 3)
         {
             GameForteController.Instance.SetStartMode(tempMode);
